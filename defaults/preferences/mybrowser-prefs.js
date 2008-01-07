b21b732ae490771637439365529366fd0c70baf4
kelson42 <kelson42@users.sourceforge.net>
2008-01-07 15:31:52 +0000

    + further removings

--- defaults/preferences/mybrowser-prefs.js
+++
@@ -1,4 +0,0 @@
-pref("toolkit.defaultChromeURI", "chrome://interfacewiki/content/interfacewiki.xul");
-pref("general.skins.selectedSkin", "classicBlue");
-pref("intl.charset.default", "UTF-8");
-pref("network.protocol-handler.warn-external.http", false);
