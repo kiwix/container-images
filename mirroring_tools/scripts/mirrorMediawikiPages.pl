a7273d19bb082159a8ee2076ad7686d4578a3903
kelson42 <kelson42@users.sourceforge.net>
2008-05-11 17:51:00 +0000

    i+ better usage function

--- mirroring_tools/scripts/mirrorMediawikiPages.pl
+++ mirroring_tools/scripts/mirrorMediawikiPages.pl
@@ -22,8 +22,18 @@ my $destinationPath;
 my $destinationUsername;
 my $destinationPassword;
 
+my $ignoreTemplateDependences;
+my $ignoreImageDependences;
+
+my $checkCompletedPages;
+my $checkCompletedImages;
+
+my $noTextMirroring;
+
 my @pages;
 my $readFromStdin = 0;
+my $noResume = 0;
+my $noLog = 0;
 
 ## Get console line arguments
 GetOptions(
@@ -37,10 +47,35 @@ GetOptions(
            'destinationPassword=s' => \$destinationPassword,
            'readFromStdin' => \$readFromStdin,
            'page=s' => \@pages,
+           'checkCompletedImages' => \$checkCompletedImages,
+           'checkCompletedPages' => \$checkCompletedPages,
+           'noResume' => \$noResume,
+           'noLog' => \$noLog,
+           'ignoreTemplateDependences' => \$ignoreTemplateDependences,
+           'ignoreImageDepedences' => \$ignoreImageDependences,
+           'noTextMirroring' => \$noTextMirroring,
            );
 
 if (!$sourceHost || !$destinationHost ) {
-    print "usage: ./mirrorMediawikiPages.pl --sourceHost=my.source.host --destinationHost=my.dest.host [--sourceUsername=my_user] [--sourcePassword=my_password] [--destinationUsername=my_user] [--destinationPassword=my_password] [--sourcePath=w]  [--destinationPath=w] [--page=my_page] [--readFromStdin]\n";
+    print "Usage:\n\t";
+    print "/mirrorMediawikiPages.pl --sourceHost=[host] --destinationHost=[host]\n\n";
+
+    print "Options:\n\t";
+    print "--sourceUsername=[username] (example: foobar)\n\t\tUsername for the source Mediawiki\n\t";
+    print "--sourcePassword=[password] (example: foobarpass)\n\t\tPassword for the source Mediawiki\n\t";
+    print "--destinationUsername=[username] (example: foobar)\n\t\tUsername for the destination Mediawiki\n\t";
+    print "--destinationPassword=[password] (example: foobarpass)\n\t\tPassword for the destination Mediawiki\n\t";
+    print "--sourcePath=[path] (example: w)\n\t\tPath in the URL to access to the source Mediawiki root\n\t";
+    print "--destinationPath=[path] (example: w)\n\t\tPath in the URL to access to the destination Mediawiki root\n\t";
+    print "--page=[page] (for example:Paris)\n\t\tPage name of the article you want to mirror. Can be used many time.\n\t";
+    print "--readFromStdin\n\t\tThe page names will be read as a carriage return separated list from STDIN.\n\t\tBe careful, you have to set the necessary passowrds in the command line if you want to use this option.\n\t";
+    print "--checkCompletedPages\n\t\tPages and templates which are already present in the destination Mediawiki will be mirrored.\n\t";
+    print "--checkCompletedImages\n\t\tImage which are alreadz present in the destination Mediawiki will be mirrored.\n\t";
+    print "--noResume\n\t\tDo not print on STDOUT a resume of the queue status.\n\t";
+    print "--noLog\n\t\tDo not start a logging process.\n\t";
+    print "--ignoreTemplateDependences\n\t\tDo not check for each downloaded page the text dependences (templates).\n\t";
+    print "--ignoreImageDependences\n\t\tDo not check for each downloaded page the image dependences (images included in the page).\n\t";
+    print "--noTextMirroring\n\t\tDo not mirror any text. Can be useful to mirror only images (for example by ginving a list of picture pages).\n";
     exit;
 }
 
@@ -59,14 +94,15 @@ if ($readFromStdin) {
     }
 }
 
-# log
-use Log::Log4perl;
-Log::Log4perl->init("../conf/log4perl");
-my $logger = Log::Log4perl->get_logger("mirrorMediawikiPages.pl");
-
-# mirror
 my $mirror = new MediaWiki::Mirror();
-$mirror->logger($logger);
+
+# log
+unless ($noLog) {
+    use Log::Log4perl;
+    Log::Log4perl->init("../conf/log4perl");
+    my $logger = Log::Log4perl->get_logger("mirrorMediawikiPages.pl");
+    $mirror->logger($logger);
+}
 
 $mirror->sourceMediawikiHost($sourceHost);
 $mirror->sourceMediawikiPath($sourcePath);
@@ -77,13 +113,24 @@ $mirror->destinationMediawikiPath($destinationPath);
 $mirror->destinationMediawikiUsername($destinationUsername);
 $mirror->destinationMediawikiPassword($destinationPassword);
 
+$mirror->checkCompletedPages($checkCompletedPages);
+$mirror->checkCompletedImages($checkCompletedImages);
+
+$mirror->checkImageDependences( $ignoreImageDependences ? 0 : 1);
+$mirror->checkTemplateDependences( $ignoreTemplateDependences ? 0 : 1);
+
+$mirror->noTextMirroring($noTextMirroring);
+
+$mirror->startMirroring();
+
 foreach my $page (@pages) {
     $mirror->addPagesToMirror($page);
 }
 
-$mirror->startMirroring();
 $mirror->wait();
 
-print $mirror->getQueueStatus();
+unless ($noResume) {
+    print $mirror->getQueueStatus();
+}
 
 exit;
