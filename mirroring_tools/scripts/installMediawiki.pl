91f45af408becce4326c42caecb533afd34e9c6d
kelson42 <kelson42@users.sourceforge.net>
2008-07-06 16:36:22 +0000

    + interactive console to enter passwords

--- mirroring_tools/scripts/installMediawiki.pl
+++ mirroring_tools/scripts/installMediawiki.pl
@@ -10,6 +10,7 @@ use Getopt::Long;
 use Data::Dumper;
 use LWP::UserAgent;
 use MediaWiki::Install;
+use Term::Query qw( query query_table query_table_set_defaults query_table_process );
 
 # log
 use Log::Log4perl;
@@ -26,6 +27,7 @@ my $sysopUser;
 my $sysopPassword;
 my $dbUser;
 my $dbPassword;
+my @confIncludes;
 
 ## Get console line arguments
 GetOptions('site=s' => \$site,
@@ -36,12 +38,21 @@ GetOptions('site=s' => \$site,
 	   'sysopUser=s' => \$sysopUser,
 	   'sysopPassword=s' => \$sysopPassword,
 	   'dbUser=s' => \$dbUser,
-	   'dbPassword=s' => \$dbPassword
+	   'dbPassword=s' => \$dbPassword,
+	   'confInclude=s@' => \@confIncludes
 	   );
 
 if (!$site || !$code || !$directory || !$languageCode || !$sysopUser || !$sysopPassword || !$dbUser || !$dbPassword) {
-    print "usage: ./installMediawiki.pl [--path=w] --directory=/var/www/mirror/fr --site=fr.kiwix.org --code=fr --languageCode=fr --sysopUser=Kelson --sysopPassword=*** --dbUser=mydbuser --dbPassword=****\n";
+    print "usage: ./installMediawiki.pl [--path=w] --directory=/var/www/mirror/fr --site=fr.kiwix.org --code=fr --languageCode=fr --sysopUser=Kelson --sysopPassword=*** --dbUser=mydbuser --dbPassword=**** [--confInclude=addconf.php]\n";
     exit;
+}
+
+if (!$sysopPassword) {
+    $sysopPassword = query("sysopPassword:", "");
+}
+
+if (!$dbPassword) {
+    $dbPassword = query("dbPassword:", "");
 }
 
 my $installer = MediaWiki::Install->new();
@@ -55,5 +66,6 @@ $installer->sysopUser($sysopUser);
 $installer->sysopPassword($sysopPassword);
 $installer->dbUser($dbUser);
 $installer->dbPassword($dbPassword);
+$installer->confIncludes(@confIncludes);
 $installer->install();
 
