7c7b6c110f96cb512a30ddabab6c7ac264575a06
kelson42 <kelson42@users.sourceforge.net>
2008-07-08 13:01:54 +0000

    + exit if a step failed

--- extern/zeno/run.sh
+++ extern/zeno/run.sh
@@ -8,21 +8,21 @@ export LD_LIBRARY_PATH="$MY_PATH/cxxtools/src/unit/.libs/:$MY_PATH/cxxtools/src/
 export PATH="$PATH:$MY_PATH/cxxtools/:$MY_PATH/tntnet/sdk/tools/ecppc/"
 
 rm -r tntnet-1.6.3.tar.gz tntnet
-wget --continue http://www.tntnet.org/download/tntnet-1.6.3.tar.gz
+wget --continue http://www.tntnet.org/download/tntnet-1.6.3.tar.gz || exit
 tar -xvzf tntnet-1.6.3.tar.gz
 mv tntnet-1.6.3 tntnet
 
 rm -rf cxxtools-1.4.8.tar.gz cxxtools
-wget --continue http://www.tntnet.org/download/cxxtools-1.4.8.tar.gz
+wget --continue http://www.tntnet.org/download/cxxtools-1.4.8.tar.gz || exit
 tar -xvzf cxxtools-1.4.8.tar.gz
 mv  cxxtools-1.4.8 cxxtools
 
 rm -rf tntdb-1.0.1.tar.gz tntdb
-wget --continue http://www.tntnet.org/download/tntdb-1.0.1.tar.gz
+wget --continue http://www.tntnet.org/download/tntdb-1.0.1.tar.gz || exit
 tar -xvzf tntdb-1.0.1.tar.gz
 mv tntdb-1.0.1 tntdb
 
-svn co https://tntzenoreader.svn.sourceforge.net/svnroot/tntzenoreader/trunk tntzenoreader
+svn co https://tntzenoreader.svn.sourceforge.net/svnroot/tntzenoreader/trunk tntzenoreader || exit
 
 for PROJ in `echo "cxxtools tntnet tntdb tntzenoreader"` ; do
   echo "== $PROJ ==============================================";
@@ -31,12 +31,11 @@ for PROJ in `echo "cxxtools tntnet tntdb tntzenoreader"` ; do
 
   if [ "$PROJ" = "tntdb" ]; then
       echo "./configure --without-postgresql --without-mysql --without-oracle --without-doxygen"
-    ./configure --without-postgresql --without-mysql --without-oracle --without-doxygen 
+    ./configure --without-postgresql --without-mysql --without-oracle --without-doxygen || exit
   else
-    ./configure
+    ./configure || exit
   fi
 
-  make clean
-  make
+  make || exit
   cd ..
 done
