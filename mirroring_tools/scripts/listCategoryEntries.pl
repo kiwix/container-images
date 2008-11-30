a1085ed3601c76d977fae733681d66c7cf707d26
kelson42 <kelson42@users.sourceforge.net>
2008-11-30 18:12:51 +0000

    + first version

---
+++ mirroring_tools/scripts/listCategoryEntries.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+binmode STDOUT, ":utf8";
+binmode STDIN, ":utf8";
+
+use utf8;
+use lib "../";
+use lib "../Mediawiki/";
+
+use Config;
+use strict;
+use warnings;
+use Getopt::Long;
+use Data::Dumper;
+use MediaWiki;
+
+# log
+use Log::Log4perl;
+Log::Log4perl->init("../conf/log4perl");
+my $logger = Log::Log4perl->get_logger("listCategoryEntries.pl");
+
+# get the params
+my $host = "";
+my $path = "";
+my $category = "";
+my $explorationDepth = 1;
+my $namespace;
+
+## Get console line arguments
+GetOptions('host=s' => \$host, 
+	   'path=s' => \$path,
+	   'category=s' => \$category,
+	   'explorationDepth=s' => \$explorationDepth,
+	   'namespace=s' => \$namespace,
+	   );
+
+if (!$host || !$category) {
+    print "usage: ./listCategoryEntries.pl --host=my.wiki.org --category=mycat [--path=w] [--explorationDepth=1] [--namespace=0]\n";
+    exit;
+}
+
+my $site = MediaWiki->new();
+$site->hostname($host);
+$site->path($path);
+$site->logger($logger);
+my @entries = $site->listCategoryEntries($category, $explorationDepth, $namespace);
+
+foreach my $entry (@entries) {
+    print $entry."\n";
+}
+
+exit;
