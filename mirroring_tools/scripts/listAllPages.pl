2b5e7531d96bcaaa73c782eac7ce6ace66353aee
kelson42 <kelson42@users.sourceforge.net>
2008-12-13 12:36:42 +0000

    + "filter" param to allow more pricesion in the request

--- mirroring_tools/scripts/listAllPages.pl
+++ mirroring_tools/scripts/listAllPages.pl
@@ -21,15 +21,17 @@ my $logger = Log::Log4perl->get_logger("listAllPages.pl");
 my $host = "";
 my $path = "";
 my $namespace;
+my $filter = "all";
 
 ## Get console line arguments
 GetOptions('host=s' => \$host, 
 	   'path=s' => \$path,
+	   'filter=s' => \$filter,
 	   'namespace=s' => \$namespace
 	   );
 
-if (!$host) {
-    print "usage: ./listAllPages.pl --host=my.wiki.org [--path=w] [--namespace=0]\n";
+if (!$host || !($filter eq "all" || $filter eq "nonredirects" || $filter eq "redirects")) {
+    print "usage: ./listAllPages.pl --host=my.wiki.org [--path=w] [--namespace=0] [--filter=[all|redirects|nonredirects]]\n";
     exit;
 }
 
@@ -37,7 +39,7 @@ my $site = MediaWiki->new();
 $site->hostname($host);
 $site->path($path);
 
-foreach my $page ($site->allPages()) {
+foreach my $page ($site->allPages($namespace, $filter)) {
     print $page."\n";
 }
 
