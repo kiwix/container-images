9eda429db8ab8710707c3689710ee2d4798c6010
kelson42 <kelson42@users.sourceforge.net>
2008-12-29 18:16:19 +0000

    + exception for external http links

--- mirroring_tools/data/skins/Parser.php
+++ mirroring_tools/data/skins/Parser.php
@@ -3,7 +3,11 @@ include("skins/ParserOriginal.php");
 
 class Parser extends ParserOriginal
 {
-	function doMagicLinks( $text ) {
-	  return $text;
+       function magicLinkCallback( $m ) {
+          # Free external link
+	  if ( isset( $m[3] ) && strval( $m[3] ) !== '' ) {
+	    return $this->makeFreeExternalLink( $m[0] );
+	  }
+	  return $m[0];
 	}
 }
