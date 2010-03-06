3de0e41c5843269a4c7b9b34fb4e6d5f51314b08
kelson42 <kelson42@users.sourceforge.net>
2010-03-06 08:46:07 +0000

    ++

--- mirroring_tools/scripts/buildFrenchBilingualDictionary.pl
+++ mirroring_tools/scripts/buildFrenchBilingualDictionary.pl
@@ -12,6 +12,7 @@ use warnings;
 use Getopt::Long;
 use Data::Dumper;
 use MediaWiki;
+use List::Compare;
 
 # log
 use Log::Log4perl;
@@ -23,18 +24,25 @@ my $host = "fr.wiktionary.org";
 my $path = "w";
 my $category = "";
 my $code = "";
+my $allFrenchWordsFile = "";
 
 # words
-my @allFrenchWords;
-my @allLangWords;
-my @allEmbeddedIns;
+my $allFrenchWords;
+my $allLangWords;
+my $allEmbeddedIns;
+my $frenchWords;
+my $langWords;
+my %frenchDictionary;
+my %langDictionary;
 
 # get console line arguments
 GetOptions('code=s' => \$code, 
-	   'category=s' => \$category);
+	   'category=s' => \$category,
+	   'allFrenchWordsFile=s' => \$allFrenchWordsFile
+    );
 
 if (!$code || !$category) {
-    print "usage: ./buildFrenchBilingualDictionary.pl --code=ses --category=songhaï_koyraboro_senni\n";
+    print "usage: ./buildFrenchBilingualDictionary.pl --code=ses --category=songhaï_koyraboro_senni [--allFrenchWordsFile=file.lst]\n";
     exit;
 }
 
@@ -44,23 +52,70 @@ $site->path($path);
 $site->logger($logger);
 
 sub getAllFrenchWords() {
-    @allFrenchWords = $site->listCategoryEntries("français", 1, "0");
+    $allFrenchWords = [$site->listCategoryEntries("français", 1, "0")];
 }
 
 sub getAllLangWords() {
-    @allLangWords = $site->listCategoryEntries($category, 1, "0");
+    $allLangWords = [$site->listCategoryEntries($category, 1, "0")];
 }
 
 sub getAllEmbeddedIns() {
-    @allEmbeddedIns = $site->embeddedIn("template:$code", "0");
+    $allEmbeddedIns = [$site->embeddedIn("template:$code", "0")];
 }
 
-#getAllFrenchWords();
+sub read_file {
+    my $file = shift;
+    my @list;
+ 
+    open(FILE, '<:utf8', $file);
+    while (my $page = <FILE>) {
+	$page =~ s/\n//;
+	push(@list, $page);
+    }
+ 
+    return \@list;
+}
+
+sub getFrenchWords() {
+    my $lc = List::Compare->new(
+	$allFrenchWords, 
+	$allEmbeddedIns
+	);
+    $frenchWords = [$lc->get_intersection()];
+}
+
+sub getLangWords() {
+    my $lc = List::Compare->new(
+	$allLangWords, 
+	$allEmbeddedIns
+	);
+    $langWords = [$lc->get_intersection()];
+}
+
+sub buildFrenchDictionary() {
+    foreach my $frenchWord (@$frenchWords) {
+	my ($content, $revision) = $site->downloadPage($frenchWord);
+	
+	if ($content =~ /\{\{trad\|$code\|([^\}]*)\}\}/) {
+	    my $langWord = $1;
+	    $frenchDictionary{$frenchWord} = { "translation" => $langWord}
+      }
+    }
+}
+
+if ($allFrenchWordsFile) {
+    $allFrenchWords = read_file($allFrenchWordsFile);
+} else {
+    getAllFrenchWords();
+}
+
 getAllLangWords();
 getAllEmbeddedIns();
+getFrenchWords();
+getLangWords();
+buildFrenchDictionary();
 
-foreach my $word (@allEmbeddedIns) {
-    print $word."\n";
-}
+print Dumper(%frenchDictionary);
 
 exit;
+
