a65ffa9312f963cb56ea6d57555ec01f37cca3be
kelson42 <kelson42@users.sourceforge.net>
2010-05-05 09:42:59 +0000

    + UsabilityInitiative in the black list
    M    Code.pm

--- mirroring_tools/Mediawiki/MediaWiki/Code.pm
+++ mirroring_tools/Mediawiki/MediaWiki/Code.pm
@@ -15,7 +15,7 @@ my $doc;
 my $mediawikiRevision = "head";
 my @extensions;
 my $filter=".*";
-my @extensionsToIgnore = ('MakeBot', 'SiteMatrix', 'FixedImage', 'OggHandler', 'BoardVote', 'CentralNotice', 'TorBlock', 'Central.*Auth', 'TitleKey', 'CheckUser', 'Cross.*namespace.*', 'GlobalBlocking', 'Global[ |_]*Usage', 'OAIRepository', 'SimpleAntiSpam', 'SpamBlacklist', 'ConfirmEdit', 'MakeBot', 'AntiBot', 'AntiSpoof', 'Oversight', 'Makesysop', 'Title.*Blacklist', 'DismissableSiteNotice', 'Username.*Blacklist', 'MWSearch', 'OpenSearchXml', 'Renameuser', 'TrustedXFF', 'Collection', 'SecurePoll', 'Abuse[ ]*Filter', 'UsabilityInitiative', 'PDF Handler', 'PrefStats', 'OptIn', 'UploadBlacklist', 'WikimediaMessages', 'NewUserMessage', 'LocalisationUpdate', 'nuke', 'FlaggedRevs' );
+my @extensionsToIgnore = ('MakeBot', 'SiteMatrix', 'FixedImage', 'OggHandler', 'BoardVote', 'CentralNotice', 'TorBlock', 'Central.*Auth', 'TitleKey', 'CheckUser', 'Cross.*namespace.*', 'GlobalBlocking', 'Global[ |_]*Usage', 'OAIRepository', 'SimpleAntiSpam', 'SpamBlacklist', 'ConfirmEdit', 'MakeBot', 'AntiBot', 'AntiSpoof', 'Oversight', 'Makesysop', 'Title.*Blacklist', 'DismissableSiteNotice', 'Username.*Blacklist', 'MWSearch', 'OpenSearchXml', 'Renameuser', 'TrustedXFF', 'Collection', 'SecurePoll', 'Abuse[ ]*Filter', 'UsabilityInitiative', 'PDF Handler', 'PrefStats', 'OptIn', 'UploadBlacklist', 'WikimediaMessages', 'NewUserMessage', 'LocalisationUpdate', 'nuke', 'FlaggedRevs', 'UsabilityInitiative' );
 
 sub new {
     my $class = shift;
