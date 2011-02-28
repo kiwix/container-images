55ebc3010d9861a669909d18dd4c03edd4930c40
kelson42 <kelson42@users.sourceforge.net>
2011-02-28 10:15:46 +0000

    remove the --linkFiles option which is not really handful

--- maintenance_tools/kiwix_superseed.sh
+++ maintenance_tools/kiwix_superseed.sh
@@ -4,28 +4,12 @@ TORRENT_DIR=/home/kelson/rtorrent/watch/
 INBOX_DIR=/home/kelson/rtorrent/inbox/
 SOURCE_DIR=/var/www/download.kiwix.org/
 WEB_DIR=http://download.kiwix.org/
-SYNC_DIRS="archive portable zim/0.9"
-
-if [ ! "$1" == "--linkFiles" ]
-then
-    INBOX_DIR=
-fi
+SYNC_DIRS="archive portable zim/0.9 other"
 
 for DIR in $SYNC_DIRS
 do
     for FILE in `rsync -az download.kiwix.org::download.kiwix.org/$DIR/ | sed -e 's/^.* //g' | grep '\....'`
     do
-
-	cd $TORRENT_DIR
-	TORRENT_FILE=$WEB_DIR$DIR/$FILE.torrent
-	LOCAL_FILE=`echo $FILE.torrent | sed -e 's/.*\///m'`
-	if [ -e "$LOCAL_FILE" ]
-	then
-	    echo "Skipping $TORRENT_FILE..."
-	else
-	    echo "Downloading $TORRENT_FILE..."
-	    wget "$TORRENT_FILE"
-	fi
 
 	if [ ! $INBOX_DIR == "" ]
 	then
@@ -40,5 +24,17 @@ do
 		ln "$SOURCE_LINK"  
 	    fi
 	fi
+
+	cd $TORRENT_DIR
+	TORRENT_FILE=$WEB_DIR$DIR/$FILE.torrent
+	LOCAL_FILE=`echo $FILE.torrent | sed -e 's/.*\///m'`
+	if [ -e "$LOCAL_FILE" ]
+	then
+	    echo "Skipping $TORRENT_FILE..."
+	else
+	    echo "Downloading $TORRENT_FILE..."
+	    wget "$TORRENT_FILE"
+	fi
+
     done
 done
