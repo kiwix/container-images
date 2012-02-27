752c010292b7dd65b24565169d88ac944da02411
kelson42 <kelson42@users.sourceforge.net>
2012-02-27 16:35:24 +0000

    + new zim fiels

--- maintenance_tools/kiwix-serve.sh
+++ maintenance_tools/kiwix-serve.sh
@@ -31,3 +31,4 @@ nice -10 kiwix-serve --port=4223 --index="wikipedia_ur_all_04_2011.zim.idx" --da
 nice -10 kiwix-serve --port=4224 --index="wikipedia_sw_all_04_2011.zim.idx" --daemon "/var/www/download.kiwix.org/zim/0.9/wikipedia_sw_all_04_2011.zim"
 nice -10 kiwix-serve --port=4225 --index="wikipedia_ru_all_05_2011.zim.idx" --daemon "/var/www/download.kiwix.org/zim/0.9/wikipedia_ru_all_05_2011.zim"
 nice -10 kiwix-serve --port=4226 --index="wikipedia_en_simple_all_08_2011.zim.idx" --daemon "/var/www/download.kiwix.org/zim/0.9/wikipedia_en_simple_all_08_2011.zim"
+nice -10 kiwix-serve --port=4227 --index="wikipedia_da_all_02_2012.zim.idx" --daemon "/var/www/download.kiwix.org/zim/0.9/wikipedia_da_all_02_2012.zim"
