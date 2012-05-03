3bd96d3d25d13e4ea28e8aaf43d89bd191a3768d
reg_ <reg_@users.sourceforge.net>
2012-05-03 16:03:18 +0000

    added buildbot configuration file

---
+++ buildbot/secret_pass.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+WINXP64_PASS = 'xxx'
+WINXP32_PASS = 'xxx'
+UBUNTU32_PASS = 'xxx'
+UBUNTU64_PASS = 'xxx'
+MACOSX_PASS = 'xxx'
+FEDORA9_PASS = 'xxx'
+ARM_PASS = 'xxx'
