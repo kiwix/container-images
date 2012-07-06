c0bbe392c0d32836746ac56669127aa802fb1e0d
kelson42 <kelson42@users.sourceforge.net>
2012-07-06 08:32:23 +0000

    remove outdated tools

--- download-platform_tools/scripts/metalink.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = $HTTP_GET_VARS[file];
-header('Content-Type: application/metalink+xml');
-header('Content-Disposition: attachment; filename="'.$file.'.metalink"');
-readfile("http://www.kiwix.org/index.php?title=Metalink/".$file."&action=raw");
-?>
