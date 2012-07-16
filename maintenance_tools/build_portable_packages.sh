c3aba901519ba66518fe9e771b7502a4533548de
kelson42 <kelson42@users.sourceforge.net>
2012-07-16 06:18:09 +0000

    + small improvement in filename computation

--- maintenance_tools/build_portable_packages.sh
+++ maintenance_tools/build_portable_packages.sh
@@ -3,7 +3,7 @@
 TARGET_DIR=/var/www/download.kiwix.org/portable/
 SCRIPT=/var/www/kiwix/tools/scripts/buildDistributionFile.pl
 SYNC_DIRS="zim/0.9"
-KIWIX_VERSION=`ls -la /var/www/download.kiwix.org/bin/unstable | cut -d " " -f10 | sed -e 's/_/-/g'` 
+KIWIX_VERSION=`ls -la /var/www/download.kiwix.org/bin/unstable | cut -d " " -f10 | sed -e 's/_/-/g' | sed -e 's/\///g'` 
 
 cd /var/www/kiwix/tools/scripts
 
@@ -12,7 +12,8 @@ do
     for FILE in `rsync -az download.kiwix.org::download.kiwix.org/$DIR/ | sed -e 's/^.* //g' | grep '\....' | grep zim`
     do
 	FILENAME="kiwix-"$KIWIX_VERSION+`echo $FILE| sed -e 's/zim/zip/g'`
-	
+	echo $FILENAME
+	exit
 	if [ ! -f "$TARGET_DIR"/"$FILENAME" ]
 	then
 	    echo "Building $FILENAME..."
