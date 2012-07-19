d3865214c70d92eec9433ab3bb19dd102421ec5c
kelson42 <kelson42@users.sourceforge.net>
2012-07-19 08:54:14 +0000

    + improved version (without using rsync)

--- maintenance_tools/kiwix_superseed.sh
+++ maintenance_tools/kiwix_superseed.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+SOURCE_DIR=$1
+
+if [ ! -e "$SOURCE_DIR" ]
+then
+    echo "You have to give the web download directory root like /var/www/download.kiwix.org/"
+    exit 1
+fi 
+
 TORRENT_DIR=/home/kelson/rtorrent/watch/
 INBOX_DIR=/home/kelson/rtorrent/inbox/
-SOURCE_DIR=/var/www/download.kiwix.org/
 WEB_DIR=http://download.kiwix.org/
 SYNC_DIRS="archive/kiwix archive/moulinwiki portable zim/0.9 other"
 
 for DIR in $SYNC_DIRS
 do
-    for FILE in `rsync -az download.kiwix.org::download.kiwix.org/$DIR/ | sed -e 's/^.* //g' | grep '\....'`
+    for FILE in `ls -1 -F $SOURCE_DIR/$DIR/ | grep -v "ignore" | grep -v "~" | sed -e 's/^.* //g' | grep '\....'`
     do
-
 	if [ ! $INBOX_DIR == "" ]
 	then
-	    SOURCE_LINK=$SOURCE_DIR$DIR/$FILE
+	    SOURCE_LINK=$SOURCE_DIR/$DIR/$FILE
 	    LOCAL_FILE=`echo $FILE | sed -e 's/.*\///m'`
 	    cd $INBOX_DIR
 	    if [ -e $LOCAL_FILE ]
@@ -26,7 +32,7 @@ do
 	fi
 
 	cd $TORRENT_DIR
-	TORRENT_FILE=$WEB_DIR$DIR/$FILE.torrent
+	TORRENT_FILE=$WEB_DIR/$DIR/$FILE.torrent
 	LOCAL_FILE=`echo $FILE.torrent | sed -e 's/.*\///m'`
 	if [ -e "$LOCAL_FILE" ]
 	then
