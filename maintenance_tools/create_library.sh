ec52845d1195e132b7fb7e5d3a4ef2e7636217b5
kelson42 <kelson42@users.sourceforge.net>
2013-08-04 05:43:45 +0200

    + small additional check

--- maintenance_tools/create_library.sh
+++ maintenance_tools/create_library.sh
@@ -11,10 +11,12 @@ cd $ZIMDIR
 
 for DIR in *
 do
-    cd $ZIMDIR/$DIR
-    echo "Changing directory to $ZIMDIR/$DIR ..."
-    for FILE in `find . -name "*zim"`
-    do
+    if [ -d $ZIMDIR/$DIR ]
+    then
+      cd $ZIMDIR/$DIR
+      echo "Changing directory to $ZIMDIR/$DIR ..."
+      for FILE in `find . -name "*zim"`
+      do
 	FILE=`echo "$FILE" | sed -e "s/\.\///"`
 	grep $FILE .ignore > /dev/null
 	if [ "$?" -eq "1" ]
@@ -22,5 +24,6 @@ do
 	    echo "Inserting $FILE ..."
 	    kiwix-manage $LIBRARYFILE add $FILE --zimPathToSave="" --url=http://download.kiwix.org/zim/$DIR/$FILE.meta4
 	fi
-    done
+      done
+    fi
 done
