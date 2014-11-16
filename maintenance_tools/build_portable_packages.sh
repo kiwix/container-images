df4b6b22df40123c640a35b2b90912247fcd69d1
kelson42 <kelson42@user.sourceforge.net>
2014-11-16 19:02:45 +0100

    + Fix

--- maintenance_tools/build_portable_packages.sh
+++ maintenance_tools/build_portable_packages.sh
@@ -6,7 +6,9 @@ TMP=/media/data/prod/kiwix-maintenance/maintenance_tools/tmp/
 SCRIPT=/media/data/prod/kiwix-tools/tools/scripts/buildDistributionFile.pl
 VERSION=`readlink /var/www/download.kiwix.org/bin/unstable | sed -e 's/_/-/g' | sed -e 's/\///g'` 
 EXCLUDE="0.9"
+
 SOURCE_ESC=`echo "$SOURCE" | sed 's/\//\\\\\//g'`
+
 
 for DIR in `find "$SOURCE" -type d | sed "s/$SOURCE_ESC//" | grep -v "$EXCLUDE"`
 do
@@ -15,13 +17,13 @@ do
 
     if [ ! -d "$ZIPTARGET$DIR" ]
     then
-	echo "Creating directory '$ZIPTARGET$DIR'"
+	echo "Creating ZIP directory '$ZIPTARGET$DIR'"
 	mkdir -p "$ZIPTARGET$DIR"
     fi
 
     if [ ! -d "$ZIMTARGET$DIR" ]
     then
-	echo "Creating directory '$ZIMTARGET$DIR'"
+	echo "Creating ZIM directory '$ZIMTARGET$DIR'"
 	mkdir -p "$ZIMTARGET$DIR"
     fi
 
@@ -32,7 +34,7 @@ do
 	then
 	    echo "Building $ZIPFILE..."
 	    cd `dirname "$SCRIPT"`
-	    $SCRIPT --filePath="$TMP$ZIPFILE" --zimPath="$SOURCE$DIR/$ZIMFILE" --tmpDirectory="$TMP" --type=portable
+	    $SCRIPT --filePath="$TMP$ZIPFILE" --zimPath="$SOURCE$DIR/$ZIMFILE" --tmpDirectory="$TMP" --type=portable --downloadMirror=download_dev_mirror
     
 	    echo "Move $TMP$ZIPFILE to $ZIPTARGET$DIR"
 	    mv "$TMP$ZIPFILE" "$ZIPTARGET$DIR"
