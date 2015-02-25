45e81089add29dfd1613159b1fe95eac131506ae
Kelson42 <kelson42@users.sourceforge.net>
2015-02-25 21:14:59 +0100

    Better wiki update message

---
+++ maintenance_tools/watcherBot.js
@@ -0,0 +1,101 @@
+#!/bin/sh
+":" //# -*- mode: js -*-; exec /usr/bin/env node --max-old-space-size=1900 --stack-size=4096 "$0" "$@"
+
+"use strict";
+
+/* INCLUDES */
+var twitter = require( 'twitter' );
+var irc = require( 'irc' );
+var rssWatcher = require( 'rss-watcher' );
+var htmlToText = require( 'html-to-text' );
+var yargs = require( 'yargs' );
+
+/* Arguments */
+var argv = yargs.usage( 'Feed #kiwix Freenode IRC channels in real-time with Sourceforge, twitter, wikis activities: $0' )
+    .require( ['consumerKey', 'consumerSecret', 'accessTokenKey', 'accessTokenSecret' ] )
+    .argv;
+
+/* VARIABLES */
+var lastTwitterId;
+var ircClient
+var client = new twitter({
+    consumer_key: argv.consumerKey,
+    consumer_secret: argv.consumerSecret,
+    access_token_key: argv.accessTokenKey,
+    access_token_secret: argv.accessTokenSecret
+});
+var kiwixWikiFeed = 'http://www.kiwix.org/w/api.php?hidebots=1&days=7&limit=50&translations=filter&action=feedrecentchanges&feedformat=rss';
+var openzimWikiFeed = 'http://www.openzim.org/w/api.php?hidebots=1&days=7&limit=50&translations=filter&action=feedrecentchanges&feedformat=rss'
+var sourceforgeFeed = 'https://sourceforge.net/p/kiwix/activity/feed.rss';
+
+/* FUNCTIONS */
+function connectIrc() {
+    ircClient= new irc.Client( 'irc.freenode.net', 'WatcherBot', {
+	channels: [ '#kiwix' ],
+    });
+}
+
+function html2txt( html ) {
+    return htmlToText.fromString( html ).replace(new RegExp(/\[[^\[]*\]/), '').replace(new RegExp(/[ ]+/), '');
+}
+
+/* INIT */
+connectIrc();
+
+var kiwixWikiWatcher = new rssWatcher( kiwixWikiFeed );
+kiwixWikiWatcher.set( {feed: kiwixWikiFeed, interval: 120} );
+kiwixWikiWatcher.on( 'new article', function( article ) {
+    var message = '[WIKI] ' + html2txt( article.title ) + ' by ' + html2txt( article.author ) + ' -- ' + article.link + ' --';
+    console.log( '[MSG]' + message );
+    ircClient.say( '#kiwix', message);
+});
+kiwixWikiWatcher.run( function( error, articles ) {
+    if ( error ) {
+	console.error( '[ERROR] ' + error );
+    }
+});
+
+var openzimWikiWatcher = new rssWatcher( openzimWikiFeed );
+openzimWikiWatcher.set( {feed: openzimWikiFeed, interval: 120} );
+openzimWikiWatcher.on( 'new article', function( article ) {
+    var message = '[WIKI] ' + html2txt( article.title ) + ' by ' + html2txt( article.author ) + ' -- ' + article.link + ' --';
+    console.log( '[MSG]' + message );
+    ircClient.say( '#kiwix', message);
+});
+openzimWikiWatcher.run( function( error, articles ) {
+    if ( error ) {
+	console.error( '[ERROR] ' + error );
+    }
+});
+
+var sourceforgeWatcher = new rssWatcher( sourceforgeFeed );
+sourceforgeWatcher.set( {feed: sourceforgeFeed, interval: 120} );
+sourceforgeWatcher.on( 'new article', function( article ) {
+    var message = '[SOURCEFORGE] ' + html2txt( article.summary ) +  ' -- ' + article.link + ' --';
+    console.log( '[MSG]' + message );
+    ircClient.say( '#kiwix', message);
+});
+sourceforgeWatcher.run( function( error, articles ) {
+    if ( error ) {
+	console.error( '[ERROR] ' + error );
+    }
+});
+
+setInterval ( function() {
+    client.get('statuses/user_timeline', {screen_name: 'KiwixOffline', count: 1}, function( error, tweets, response ) {
+	if ( error ) {
+	    console.error( '[ERROR] ' + error.essage );
+	} else if ( !error && lastTwitterId != tweets[0].id_str ) {
+	    lastTwitterId = tweets[0].id_str;
+	    var message = '[MICROBLOG] ' + tweets[0].text + ' -- https://twitter.com/KiwixOffline/status/' + tweets[0].id_str + ' --';
+	    console.log( '[MICROBLOG]' + message );
+	    ircClient.say( '#kiwix', message);
+	}
+    })
+}, 60000 );
+
+ircClient.addListener( 'error', function( error ) {
+    console.log( '[ERROR] ' + error );
+    lastTwitterId = undefined;
+    setTimeout( connectIrc, 300000 );
+});
