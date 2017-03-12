0a47fecf9848c147ccc76c58df01f7a64f45ba20
Emmanuel Engelhart <kelson@kiwix.org>
2017-03-12 17:17:31 +0100

    Move here server conf for mwoffliner

---
+++ mwoffliner/mwoffliner3_shm.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ZIM2INDEX=/srv/upload/zim2index/
+SCRIPT=`readlink -f $0/../`
+SCRIPT_DIR=`dirname "$SCRIPT"`
+MWOFFLINER="$SCRIPT_DIR/mwoffliner.js"
+MWMATRIXOFFLINER="$SCRIPT_DIR/mwmatrixoffliner.js --speed=5 --verbose --skipHtmlCache --adminEmail=contact@kiwix.org --mwUrl=https://meta.wikimedia.org/ --tmpDirectory=/dev/shm/ --skipCacheCleaning"
+
