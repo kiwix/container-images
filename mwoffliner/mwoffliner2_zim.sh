fae39f6d87ce3b2e69f7b9f8c48f257838fbe40e
Kelson <kelson@kiwix.org>
2017-07-18 21:31:59 +0000

    skip html caching

--- mwoffliner/mwoffliner2_zim.sh
+++ mwoffliner/mwoffliner2_zim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ZIM2INDEX=/srv/upload/zim2index/
-MWMATRIXOFFLINER="mwmatrixoffliner --verbose --adminEmail=contact@kiwix.org --mwUrl=https://meta.wikimedia.org/ --deflateTmpHtml --skipCacheCleaning"
+MWMATRIXOFFLINER="mwmatrixoffliner --verbose --adminEmail=contact@kiwix.org --mwUrl=https://meta.wikimedia.org/ --deflateTmpHtml --skipCacheCleaning --skipHtmlCache"
 
 # Wikipedia
 $MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --language="(ko|hu)" &&
