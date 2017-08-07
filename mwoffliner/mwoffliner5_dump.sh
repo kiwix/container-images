6f3523caecf3ca097b4e0fb0f92d477e7e78e557
Kelson <kelson@kiwix.org>
2017-08-07 06:14:48 +0000

    Without video

--- mwoffliner/mwoffliner5_dump.sh
+++ mwoffliner/mwoffliner5_dump.sh
@@ -2,8 +2,7 @@
 
 ZIM2INDEX=/srv/upload/zim2index/
 ARGS="--adminEmail=contact@kiwix.org --deflateTmpHtml --verbose --skipHtmlCache --skipCacheCleaning"
-MWOFFLINER="mwoffliner --format= --format=nopic $ARGS"
-MWMATRIXOFFLINER="mwmatrixoffliner --mwUrl=https://meta.wikimedia.org/ $ARGS"
+MWOFFLINER="mwoffliner --format=novid --format=nopic $ARGS"
 
 # Wikipedia in English
-$MWOFFLINER --mwUrl="https://en.wikipedia.org/" --parsoidUrl="https://en.wikipedia.org/api/rest_v1/page/html/" --customMainPage="user:Popo_le_Chien/Kiwix" --outputDirectory=$ZIM2INDEX/wikipedia/
+$MWOFFLINER --mwUrl="https://en.wikipedia.org/" --parsoidUrl="https://en.wikipedia.org/api/rest_v1/page/html/" --customMainPage="User:Popo_le_Chien/Kiwix" --outputDirectory=$ZIM2INDEX/wikipedia/
