fc11ac9d284cecf525156dbbddb085e8669c64c6
Kelson <kelson@kiwix.org>
2018-07-07 10:58:03 +0200

    Move WPSR

---
+++ mwoffliner/mwoffliner4_zim.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Allows to write core file
+ulimit -c unlimited
+
+ZIM2INDEX=/srv/upload/zim2index/
+ARGS="--withZimFullTextIndex --adminEmail=contact@kiwix.org --deflateTmpHtml --verbose --skipHtmlCache --skipCacheCleaning"
+MWOFFLINER="mwoffliner --format=novid --format=nopic $ARGS"
+MWMATRIXOFFLINER="mwmatrixoffliner --mwUrl=https://meta.wikimedia.org/ $ARGS"
+
+# Wikipedia
+$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --language="(sr)"
