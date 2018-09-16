3fe289626ff57d16fde3574740eca17def3f1503
Kelson <kelson@kiwix.org>
2018-09-16 22:31:59 +0200

    Better molcell zim logo

--- mwoffliner/mwoffliner5_dump.sh
+++ mwoffliner/mwoffliner5_dump.sh
@@ -36,7 +36,7 @@ $MWOFFLINER_MOBILE --format=nodet --mwUrl="https://en.wikipedia.org/" --parsoidU
 # Molecular & Cell Biology
 /srv/kiwix-tools/tools/scripts/listCategoryEntries.pl --host=en.wikipedia.org --path=w --exploration=5 --namespace=1 --category="WikiProject_Molecular_and_Cellular_Biology_articles" | sed 's/Talk://' | sort -u > "$SCRIPT_DIR/molcell_unfiltered" &&
 grep -Fxv -f "$SCRIPT_DIR/filter_out" "$SCRIPT_DIR/molcell_unfiltered" | sort -u > "$SCRIPT_DIR/molcell" &&
-wget "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Animal_Cell_Unannotated.svg/1405px-Animal_Cell_Unannotated.svg.png" -O "$SCRIPT_DIR/molcell.png" &&
+wget "https://upload.wikimedia.org/wikipedia/commons/7/73/MolCellBiol_Kiwix.png" -O "$SCRIPT_DIR/molcell.png" &&
 $MWOFFLINER_MOBILE --format=nodet --mwUrl="https://en.wikipedia.org/" --parsoidUrl="https://en.wikipedia.org/api/rest_v1/page/html/" --customZimTitle="Wikipedia Molecular and Cell Biology" --customZimDescription="30,000 Molecular and Cell Biology articles from Wikipedia" --customMainPage="Wikipedia:WikiProject_Molecular_and_Cell_Biology/Offline" --customZimFavicon="$SCRIPT_DIR/molcell.png" --articleList="$SCRIPT_DIR/molcell" --outputDirectory=$ZIM2INDEX/wikipedia/ &&
 
 # Maths
