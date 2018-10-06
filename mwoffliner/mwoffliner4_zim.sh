197e192b38f6db7cbe1cc451e262985f225d53d2
Kelson <kelson@kiwix.org>
2018-10-06 17:39:01 +0000

    Write directly to ZIM directory

--- mwoffliner/mwoffliner4_zim.sh
+++ mwoffliner/mwoffliner4_zim.sh
@@ -3,16 +3,16 @@
 # Allows to write core file
 ulimit -c unlimited
 
-ZIM2INDEX=/srv/upload/zim2index/
+ZIM=/srv/upload/zim/
 ARGS="--withZimFullTextIndex --adminEmail=contact@kiwix.org --deflateTmpHtml --verbose --skipHtmlCache --skipCacheCleaning"
 MWOFFLINER="mwoffliner --format=novid --format=nopic $ARGS"
 MWMATRIXOFFLINER="mwmatrixoffliner --mwUrl=https://meta.wikimedia.org/ $ARGS"
 
 # Wikipedia
-$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --language="(sr)" &&
+$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM/wikipedia/ --language="(sr)" &&
 
 # Wiktionary
-$MWMATRIXOFFLINER --project=wiktionary --outputDirectory=$ZIM2INDEX/wiktionary/--language="(en|mg)" &&
+$MWMATRIXOFFLINER --project=wiktionary --outputDirectory=$ZIM/wiktionary/--language="(en|mg)" &&
 
 # Wiktionary FR
-$MWOFFLINER --mwUrl="https://fr.wiktionary.org/" --parsoidUrl="https://fr.wiktionary.org/api/rest_v1/page/html/" --customMainPage="Utilisateur:Stephane_(Kiwix)/Landing" --outputDirectory=$ZIM2INDEX/wiktionary/ &&
+$MWOFFLINER --mwUrl="https://fr.wiktionary.org/" --parsoidUrl="https://fr.wiktionary.org/api/rest_v1/page/html/" --customMainPage="Utilisateur:Stephane_(Kiwix)/Landing" --outputDirectory=$ZIM/wiktionary/ &&
