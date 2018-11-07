170da065ba83c2c5c94b256022a5ee0fb1b3491f
Florent Kaisser <florent.pro@kaisser.name>
2018-11-07 20:21:14 +0100

    set wiki password for manageContentRepository.pl as a env variable

--- library-docker/bin/start.sh
+++ library-docker/bin/start.sh
@@ -5,6 +5,17 @@ ln -fs /var/www/download.kiwix.org/zim/ /var/www/library.kiwix.org/zim
   echo "Crawl-delay: 3" ; \
 } > /var/www/library.kiwix.org/robots.txt
 
+{ \
+  echo "cd $LIBRARY_DIR" ; \
+  echo "manageLibraryKiwixOrg.pl --source=/var/www/download.kiwix.org/library/library_zim.xml > library.kiwix.org.xml" ; \
+} > /etc/cron.daily/80generateLibraryKiwixOrg && chmod 0500 /etc/cron.daily/80generateLibraryKiwixOrg
+
+{ \
+  echo "cd $LIBRARY_DIR" ; \
+  echo "manageContentRepository.pl --wikiPassword=$WIKIPASSWORD --writeWiki --writeHtaccess --writeLibrary --deleteOutdatedFiles" ; \
+} > /etc/cron.daily/10updateContentRepository && chmod 0500 /etc/cron.daily/10updateContentRepository
+
+
 echo "Generate library.kiwix.org.xml file"
 manageLibraryKiwixOrg.pl --source=/var/www/download.kiwix.org/library/library_zim.xml > library.kiwix.org.xml
 
