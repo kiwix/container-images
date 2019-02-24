dcb27c5b87726c15dbc75dc626b815f1c3f96221
Chris Li <chrisencoded@gmail.com>
2019-02-24 10:16:15 -0500

    Migrated 22 more Wikipedias

--- mwoffliner/mwoffliner2_shm.sh
+++ mwoffliner/mwoffliner2_shm.sh
@@ -7,4 +7,4 @@ ZIM=/srv/upload/zim/
 MWMATRIXOFFLINER="mwmatrixoffliner --withZimFullTextIndex --verbose --deflateTmpHtml --adminEmail=contact@kiwix.org --mwUrl=https://meta.wikimedia.org/ --tmpDirectory=/dev/shm/ --skipCacheCleaning --skipHtmlCache"
 
 # Wikipedia
-$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM/wikipedia/ --language="(bg|cs|da|et|el|eo|eu|gl|hy|hi|hr|kk|lt|la|ms|min|nn|simple|sk|sl|uk|uz)"
+# MIGRATED $MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM/wikipedia/ --language="(bg|cs|da|et|el|eo|eu|gl|hy|hi|hr|kk|lt|la|ms|min|nn|simple|sk|sl|uk|uz)"
