d2f647684bbaecab286173fa10a0d5c13fef138f
Chris Li <chrisencoded@gmail.com>
2019-02-24 14:52:55 -0500

    Migrate mwoffliner4_zim.sh

--- mwoffliner/mwoffliner4_zim.sh
+++ mwoffliner/mwoffliner4_zim.sh
@@ -9,13 +9,13 @@ MWOFFLINER="mwoffliner --format=novid --format=nopic $ARGS"
 MWMATRIXOFFLINER="mwmatrixoffliner --mwUrl=https://meta.wikimedia.org/ $ARGS"
 
 # Wikipedia
-$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM/wikipedia/ --language="(sr)" &&
+# MIGRATED $MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM/wikipedia/ --language="(sr)" &&
 
 # Wiktionary
-$MWMATRIXOFFLINER --project=wiktionary --outputDirectory=$ZIM/wiktionary/ --language="(en|mg)" &&
+# MIGRATED $MWMATRIXOFFLINER --project=wiktionary --outputDirectory=$ZIM/wiktionary/ --language="(en|mg)" &&
 
 # Wiktionary FR
-$MWOFFLINER --mwUrl="https://fr.wiktionary.org/" --parsoidUrl="https://fr.wiktionary.org/api/rest_v1/page/html/" --customMainPage="Utilisateur:Stephane_(Kiwix)/Landing" --outputDirectory=$ZIM/wiktionary/
+# MIGRATED $MWOFFLINER --mwUrl="https://fr.wiktionary.org/" --parsoidUrl="https://fr.wiktionary.org/api/rest_v1/page/html/" --customMainPage="Utilisateur:Stephane_(Kiwix)/Landing" --outputDirectory=$ZIM/wiktionary/
 
 # Wiktionary DE
-$MWOFFLINER --mwUrl="https://de.wiktionary.org/" --parsoidUrl="https://de.wiktionary.org/api/rest_v1/page/html/" --namespace=108 --outputDirectory=$ZIM/wiktionary/
+# MIGRATED $MWOFFLINER --mwUrl="https://de.wiktionary.org/" --parsoidUrl="https://de.wiktionary.org/api/rest_v1/page/html/" --namespace=108 --outputDirectory=$ZIM/wiktionary/
