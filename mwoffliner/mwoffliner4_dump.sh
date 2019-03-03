a798faadfe2b605085998a88652c302af48b2991
Chris Li <chrisencoded@gmail.com>
2019-03-03 16:56:48 -0500

    migrate mwoffliner4_dump

--- mwoffliner/mwoffliner4_dump.sh
+++ mwoffliner/mwoffliner4_dump.sh
@@ -9,22 +9,22 @@ MWOFFLINER="mwoffliner --format=novid --format=nopic $ARGS"
 MWMATRIXOFFLINER="mwmatrixoffliner --mwUrl=https://meta.wikimedia.org/ $ARGS"
 
 # Wikipedia in Arabic
-$MWOFFLINER --mwUrl="https://ar.wikipedia.org/" --parsoidUrl="https://ar.wikipedia.org/api/rest_v1/page/html/" --customMainPage="مستخدم:Stephane_(Kiwix)/Landing" --outputDirectory=$ZIM/wikipedia/ &&
+# MIGRATED $MWOFFLINER --mwUrl="https://ar.wikipedia.org/" --parsoidUrl="https://ar.wikipedia.org/api/rest_v1/page/html/" --customMainPage="مستخدم:Stephane_(Kiwix)/Landing" --outputDirectory=$ZIM/wikipedia/ &&
 
 # Wikipedia in Hebrew
-$MWOFFLINER --mwUrl="https://he.wikipedia.org/" --parsoidUrl="https://he.wikipedia.org/api/rest_v1/page/html/" --customMainPage="ויקיפדיה:עמוד_ראשי/לא-מקוון" --outputDirectory=$ZIM/wikipedia/ &&
+# MIGRATED $MWOFFLINER --mwUrl="https://he.wikipedia.org/" --parsoidUrl="https://he.wikipedia.org/api/rest_v1/page/html/" --customMainPage="ויקיפדיה:עמוד_ראשי/לא-מקוון" --outputDirectory=$ZIM/wikipedia/ &&
 
 # Wikipedia in French
-$MWOFFLINER --mwUrl="https://fr.wikipedia.org/" --parsoidUrl="https://fr.wikipedia.org/api/rest_v1/page/html/" --customMainPage="Utilisateur:Popo_le_Chien/Kiwix" --outputDirectory=$ZIM/wikipedia/ &&
+# MIGRATED $MWOFFLINER --mwUrl="https://fr.wikipedia.org/" --parsoidUrl="https://fr.wikipedia.org/api/rest_v1/page/html/" --customMainPage="Utilisateur:Popo_le_Chien/Kiwix" --outputDirectory=$ZIM/wikipedia/ &&
 
 # Wikipedia in Spanish
-$MWOFFLINER --mwUrl="https://es.wikipedia.org/" --parsoidUrl="https://es.wikipedia.org/api/rest_v1/page/html/" --customMainPage="Usuario:Popo_le_Chien/Kiwix" --outputDirectory=$ZIM/wikipedia/ &&
+# MIGRATED $MWOFFLINER --mwUrl="https://es.wikipedia.org/" --parsoidUrl="https://es.wikipedia.org/api/rest_v1/page/html/" --customMainPage="Usuario:Popo_le_Chien/Kiwix" --outputDirectory=$ZIM/wikipedia/ &&
 
 # Wikipedia TR
-$MWOFFLINER --mwUrl="https://tr.wikipedia.org/" --parsoidUrl="https://tr.wikipedia.org/api/rest_v1/page/html/" --customMainPage="Kullanıcı:Stephane_(Kiwix)/Landing" --outputDirectory=$ZIM/wikipedia/ &&
+# MIGRATED $MWOFFLINER --mwUrl="https://tr.wikipedia.org/" --parsoidUrl="https://tr.wikipedia.org/api/rest_v1/page/html/" --customMainPage="Kullanıcı:Stephane_(Kiwix)/Landing" --outputDirectory=$ZIM/wikipedia/ &&
 
 # Wikipedia DE
-$MWOFFLINER --mwUrl="https://de.wikipedia.org/" --parsoidUrl="https://de.wikipedia.org/api/rest_v1/page/html/" --customMainPage="Benutzer:The_other_Kiwix_guy/Landing" --outputDirectory=$ZIM/wikipedia/ &&
+# MIGRATED $MWOFFLINER --mwUrl="https://de.wikipedia.org/" --parsoidUrl="https://de.wikipedia.org/api/rest_v1/page/html/" --customMainPage="Benutzer:The_other_Kiwix_guy/Landing" --outputDirectory=$ZIM/wikipedia/ &&
 
 # Wikipedia
-$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM/wikipedia/ --language="(fi|no|it)"
+# MIGRATED $MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM/wikipedia/ --language="(fi|no|it)"
