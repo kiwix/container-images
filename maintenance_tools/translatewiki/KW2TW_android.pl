1b7f82e63e25f3508bd927e595f7b16f3a7d638e
Kelson <kelson@kiwix.org>
2019-07-06 22:39:42 +0200

    Re-indent a bit

--- maintenance_tools/translatewiki/KW2TW_android.pl
+++ maintenance_tools/translatewiki/KW2TW_android.pl
@@ -15,10 +15,11 @@ my $path;
 my $source;
 
 # Get console line arguments
-GetOptions('path=s' => \$path,
-	   'language=s' => \@languages,
-	   'allLanguages' => \$allLanguages
-	   );
+GetOptions(
+    'path=s' => \$path,
+    'language=s' => \@languages,
+    'allLanguages' => \$allLanguages
+    );
 
 if (!$path) {
     print STDERR "usage: ./KW2TW_android.pl --path=./ [--language=fr] [--allLanguages]\n";
@@ -30,11 +31,11 @@ if (!$path) {
 
 # Get all languages if necessary
 if ($allLanguages) {
-    opendir(DIR, $path."/app/src/main/res") || die("Cannot open directory."); 
+    opendir(DIR, $path."/app/src/main/res") || die("Cannot open directory.");
     foreach my $language (readdir(DIR)) {
-	if ($language =~ '^values-([a-z]{2,3})$') {
-	    push(@languages, $1);
-	}
+        if ($language =~ '^values-([a-z]{2,3})$') {
+            push(@languages, $1);
+        }
     }
 }
 
@@ -44,29 +45,29 @@ foreach my $language (@languages) {
     my $mainPath = $path."/app/src/main/res/values/strings.xml";
 
     # Put everything which is not android in
-    my $old = readFile($language);
+    my $old = readFile($language) || "";
     for (split(/^/, $old)) {
-	if ($_ !~ /^android\.ui/sg) {
-	    my $line = $_;
-	    $txt .= $line =~ /\n$/ ? $line : $line . "\n";
-	}
+        if ($_ !~ /^android\.ui/sg) {
+            my $line = $_;
+            $txt .= $line =~ /\n$/ ? $line : $line . "\n";
+        }
     }
-    
+
     $source = readFile($mainPath);
     while ($source =~ /<(string|item)([^\-]*?name=['|"])([^'|^"]+)(['|"][^>]*?>)(.*?)(<\/)(string|item)>/sg) {
-	my $tag = $1;
-	my $middle1 = $2;
-	my $name = $3;
-	my $middle2 = $4;
-	my $value = $5;
-	my $last = $6.$7;
-	
-	$txt .= "android.ui.$name=$value\n";
+        my $tag = $1;
+        my $middle1 = $2;
+        my $name = $3;
+        my $middle2 = $4;
+        my $value = $5;
+        my $last = $6.$7;
+
+        $txt .= "android.ui.$name=$value\n";
     }
 
     # Sort entries
-    $txt = join("\n", sort(split( /\n/, $txt))); 
-    
+    $txt = join("\n", sort(split( /\n/, $txt)));
+
     # Write TW file
     writeFile($language, $txt);
 }
@@ -74,7 +75,7 @@ foreach my $language (@languages) {
 sub writeFile {
     my $file = shift;
     my $data = shift;
-    
+
     open (FILE, ">:utf8", "$file") or die "Couldn't open file: $file";
     print FILE $data;
     close (FILE);
