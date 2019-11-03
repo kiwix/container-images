79d7f7a681073e114828d4caaa1d3cbab484168c
Kelson <kelson@kiwix.org>
2019-11-03 18:40:47 +0100

    Remove outdated scripts

--- maintenance_tools/check_mirrorbrain.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl --silent --head http://download.kiwix.org/zim/0.9/ICD10-fr.zim.mirrorlist | grep Content-Type | grep html > /dev/null ; if [[ "$?" == "1" ]] ; then if [ ! -f /tmp/mb_error ] ; then touch /tmp/mb_error; zenity --display=:1 --error --text="Error with mirrorbrain" ; rm /tmp/mb_error; fi ; fi
