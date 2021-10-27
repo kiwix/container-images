668c080d7535d19e00af67861a74cd39e677679f
Emmanuel Engelhart <kelson@kiwix.org>
2021-10-27 16:44:08 +0200

    Add drive.farm.openzim.org to orchestration

--- surfer-docker/start.sh
+++ surfer-docker/start.sh
@@ -2,6 +2,18 @@
 
 set -eu
 
+echo "Reading secrets"
+for secret in password
+do
+
+    if [ -f /run/secrets/$secret ]
+    then
+        varname=$(echo $secret | tr a-z A-Z)
+        echo "[entrypoint] exposing ${secret} secret as ${varname}"
+        export $varname=$(cat /run/secrets/$secret)
+    fi
+done
+
 echo "Configure 'admin' credentials"
 ./admin user-add --username "admin" --password "$PASSWORD" || true
 
