d2e8e444070849fd1aa3fa89e6f554d4a2d53c86
Emmanuel Engelhart <kelson@kiwix.org>
2022-04-03 17:37:40 +0200

    Remove duplicate crontab entries

--- bittorrent-superseeder/bin/start.sh
+++ bittorrent-superseeder/bin/start.sh
@@ -2,11 +2,11 @@
 
 echo "Update crontab for ZIM synchronisation"
 printf "
-@reboot root /usr/local/bin/bin/sync_superseeder.sh >> /var/log/sync_superseeder.log
-0 * * * * root /usr/local/bin/bin/sync_superseeder.sh >> /var/log/sync_superseeder.log
+@reboot root /usr/local/bin/sync_superseeder.sh >> /var/log/sync_superseeder.log
+0 * * * * root /usr/local/bin/sync_superseeder.sh >> /var/log/sync_superseeder.log
 " >> /etc/crontab
 
-service cron start && crontab /etc/crontab
+service cron start
 
 echo "Listening to cron logs..."
 sleep 2
