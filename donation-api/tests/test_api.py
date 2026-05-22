c7997c7a076bb3be6db561025c8fdb20dc752163
rgaudin <reg@rskg.org>
2026-05-22 09:48:22 +0000

    donation-api has been copied to another repo
    https://github.com/kiwix/donation-portal
    
    Issue #268

--- donation-api/tests/test_api.py
+++
@@ -1,8 +0,0 @@
-from http import HTTPStatus
-
-from fastapi.testclient import TestClient
-
-
-def test_root(client: TestClient):
-    resp = client.get("/", follow_redirects=False)
-    assert resp.status_code == HTTPStatus.PERMANENT_REDIRECT
